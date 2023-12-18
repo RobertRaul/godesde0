fc57020243e3c0bfc0b0a55fc545e8288c720763
Robert Raul <lordnebr4@gmail.com>
2023-12-18 17:11:43 -0500

    Commit .2

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"github.com/robertraul/godesde0/variables"
+	"fmt"
 )
 
 func main(){
-	variables.MuestroEnteros()
+	estado, texto := variables.ConviertoaTexto(9555)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
 
