6cb3a9b192218ac527cfe434ce300772c1b8c11a
Robert Raul <lordnebr4@gmail.com>
2023-12-18 17:53:14 -0500

    Commit Numero

---
+++ ejercicios/01.go
@@ -0,0 +1,22 @@
+package ejercicios
+
+import (	
+	"strconv"
+)
+
+func Mayor100(valor string) (int,string){
+	
+	num, err := strconv.Atoi(valor)
+
+	if err != nil{
+		return 0,"Hubo un error: " + err.Error()
+	}
+
+	if  num > 100{
+		return num,"Es mayor a 100"
+	}else{
+		return num,"Es menor a 100"
+	}	
+
+
+}
--- main.go
+++ main.go
@@ -3,27 +3,34 @@ package main
 import (
 	//"github.com/robertraul/godesde0/variables"
 	"fmt"
-	"runtime"
+	//"runtime"
+	"github.com/robertraul/godesde0/ejercicios"
 )
 
 func main(){
 	//estado, texto := variables.ConviertoaTexto(1000)
 	//fmt.Println(estado)
 	//fmt.Println(texto)	
-	if os := runtime.GOOS; os == "Linux."{
-		fmt.Println("Esto no es Windows")
-	}else{
-		fmt.Println("Esto SI ES Windows", os)
-	}
+	// if os := runtime.GOOS; os == "Linux."{
+	// 	fmt.Println("Esto no es Windows")
+	// }else{
+	// 	fmt.Println("Esto SI ES Windows", os)
+	// }
 
-	switch os := runtime.GOOS; os {
-	case "windowss":
-		fmt.Println("Estpos e s Womdpws")
-	case "Darwin":
-		fmt.Println("Estpos es Darwin")
-	default:
-		fmt.Println("\n", os)
+	// switch os := runtime.GOOS; os {
+	// case "windowss":
+	// 	fmt.Println("Estpos e s Womdpws")
+	// case "Darwin":
+	// 	fmt.Println("Estpos es Darwin")
+	// default:
+	// 	fmt.Println(os)
 		
-	}
+	// }
+
+	numero, texto := ejercicios.Mayor100("fas9")
+	fmt.Println(numero)
+	fmt.Println(texto)
+
+	
 }
 
