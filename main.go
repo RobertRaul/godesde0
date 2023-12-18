346272b93fd1fa1e46661c38ce5220b5faa84860
Robert Raul <lordnebr4@gmail.com>
2023-12-18 17:30:16 -0500

    Commit Numero

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
-	"github.com/robertraul/godesde0/variables"
+	//"github.com/robertraul/godesde0/variables"
 	"fmt"
+	"runtime"
 )
 
 func main(){
-	estado, texto := variables.ConviertoaTexto(1000)
-	fmt.Println(estado)
-	fmt.Println(texto)
+	//estado, texto := variables.ConviertoaTexto(1000)
+	//fmt.Println(estado)
+	//fmt.Println(texto)	
+	if os := runtime.GOOS; os == "Linux."{
+		fmt.Println("Esto no es Windows")
+	}else{
+		fmt.Println("Esto SI ES Windows", os)
+	}
+
+	switch os := runtime.GOOS; os {
+	case "windowss":
+		fmt.Println("Estpos e s Womdpws")
+	case "Darwin":
+		fmt.Println("Estpos es Darwin")
+	default:
+		fmt.Println("\n", os)
+		
+	}
 }
 
